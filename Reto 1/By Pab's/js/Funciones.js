be2c5e64d43656e1c602e528d1763eb583386c67
Pabs125 <79167350+Pabs125@users.noreply.github.com>
2021-02-16 15:50:38 -0500

    Add files via upload

--- Reto 1/By Pab's/js/Funciones.js
+++ Reto 1/By Pab's/js/Funciones.js
@@ -23,82 +23,178 @@ var grupo3=[
     {order:6, scrImg: "img/Grupo 3/6.jpg"}
 ]
 
-function seleccionGrupo(){
-    let grupoSeleccionado = document.getElementById("imgSelect").value;
-    if(grupoSeleccionado==="1"){
-        reordenar(grupo1);
-    }else if(grupoSeleccionado==="2"){
-        reordenar(grupo2);
-    }else if(grupoSeleccionado==="3"){
-        reordenar(grupo3);
+var selectionGroup = new Array();
+var userName = "";
+
+function realizeProcess(){
+    userName=document.getElementById("user_name").value;
+    let algSelected = document.getElementById("algoritmoSeleccionado").value;
+    let groupSelected = document.getElementById("imgSelect").value;
+    let radioButtons = document.getElementsByName("order");
+    let orderSelected = "";
+    radioButtons.forEach(i => {
+        if(i.checked){
+            orderSelected = i.value;
+        }
+    });
+    if((algSelected != "") && (groupSelected != "") && (orderSelected != "")){
+        orderingProcess(algSelected,orderSelected);
+    }else{
+        alertError(algSelected,groupSelected,orderSelected);
     }
 }
 
-function reordenar(array){
-    grupoSeleccion = array;
-    grupoSeleccion.sort(function(){return Math.random() - 0.5});
+function selectGroup(){
+    let groupSelection = document.getElementById("imgSelect").value;
+    if(groupSelection === "1"){
+        groupReorder(grupo1);
+    }else if(groupSelection === "2"){
+        groupReorder(grupo2);
+    }else if(groupSelection === "3"){
+        groupReorder(grupo3);
+    }
+}
+
+function groupReorder(array){
+    selectionGroup = array;
+    selectionGroup.sort(function() { return Math.random() - 0.5 });
     var input = document.getElementsByName('ubi[]');
-    for(var i=0; i< input.length; i++){
-        input[i].src = seleccionGrupo[i].scrImg;
+    for (var i = 0; i < input.length; i++) {
+        input[i].src = selectionGroup[i].scrImg;
     }
 }
-
-function algoritmoBurbuja1() {
-    
+function bubbleUp(){
     var aux;
-    for(let i =0;i< grupoSeleccion.length; i++){
-        for( let j=0; j < grupoSeleccion.length; j++){
-            if(grupoSeleccion[i].order < grupoSeleccion[j].order){
-                aux= grupoSeleccion[i];
-                grupoSeleccion[i] = grupoSeleccion[j];
-                grupoSeleccion[j] = aux;
-            }
-        }
+    for (let i = 0; i< selectionGroup.length; i++) {
+       for (let j = 0; j < selectionGroup.length; j++) {
+           if(selectionGroup[i].order < selectionGroup[j].order){
+               aux = selectionGroup[i];
+               selectionGroup[i] = selectionGroup[j];
+               selectionGroup[j] = aux;
+           }
+       }
     }
 }
-
-function algoritmoSeleccion() {
+function bubbleDown(){
+    var input = document.getElementsByName('ubi[]');
     var aux;
-    var min;
-    for( let i =0; i<(grupoSeleccion.length -1); i++){
-        min=i;
-        for(let j=i+1; j<grupoSeleccion.length; j++){
-            if(grupoSeleccion[j].order<grupoSeleccion[min].order){
-                min=j;
-            }
-        }
-        aux=grupoSeleccion[i];
-        grupoSeleccion[i]=grupoSeleccion[min];
-        grupoSeleccion[min]=aux;
+    for (let i = 0; i< selectionGroup.length; i++) {
+       for (let j = 0; j < selectionGroup.length; j++) {
+           if(selectionGroup[i].order > selectionGroup[j].order){
+               aux = selectionGroup[i];
+               selectionGroup[i] = selectionGroup[j];
+               selectionGroup[j] = aux;
+           }
+       }
     }
 }
 
-function algoritmoInsercion() {
+function insertionUp(){
     var v;
     var j;
-    for(let i = 1; i<grupoSeleccion.length; i++){
-        v=grupoSeleccion[i];
-        j=i-1;
-        while ((j>=0)&&(grupoSeleccion[j].order<v.order)) {
-            grupoSeleccion[j+1] = grupoSeleccion[j];
-            j=j-1;
+    for (let i = 1; i < selectionGroup.length; i++) {
+        v = selectionGroup[i];
+        j = i - 1;
+        while( (j>=0) && (selectionGroup[j].order>v.order) ){
+            selectionGroup[j+1] = selectionGroup[j];
+			j = j - 1;
         }
-        grupoSeleccion[j+1]=v;
+        selectionGroup[j+1] = v;
     }
 }
 
-function showSelected() {
-    let seleccion = document.getElementById("algoritmoSeleccionado").value;
-    document.getElementById("algSelected").innerHTML = seleccion;
-    /* Para obtener el texto que ve el usuario */
-    let selected_name = document.getElementById("algoritmoSeleccionado");
-    let opcion = selected_name.options[selected_name.selectedIndex].text;
-    alert(opcion);
-    if (seleccion === "1")
-        algoritmoBurbuja();
-    else if (seleccion === "2")
-        algoritmoInsercion();
-    else
-        algoritmoSeleccion();
-
+function insertionDown(){
+    var v;
+    var j;
+    for (let i = 1; i < selectionGroup.length; i++) {
+        v = selectionGroup[i];
+        j = i - 1;
+        while( (j>=0) && (selectionGroup[j].order<v.order) ){
+            selectionGroup[j+1] = selectionGroup[j];
+			j = j - 1;
+        }
+        selectionGroup[j+1] = v;
+    }
 }
+
+function selectionUp(){
+    var aux;
+    var min;
+	for(let i = 0; i < (selectionGroup.length - 1); i++){
+		min=i;
+		for(let j=i+1;j<selectionGroup.length;j++){
+			if(selectionGroup[j].order < selectionGroup[min].order){
+				min=j;
+			}
+		}
+        aux = selectionGroup[i];
+        selectionGroup[i] = selectionGroup[min];
+        selectionGroup[min] = aux;
+	}
+}
+
+function selectionDown(){
+    var aux;
+    var min;
+	for(let i = 0; i < (selectionGroup.length - 1); i++){
+		min=i;
+		for(let j=i+1;j<selectionGroup.length;j++){
+			if(selectionGroup[j].order > selectionGroup[min].order){
+				min=j;
+			}
+		}
+        aux = selectionGroup[i];
+        selectionGroup[i] = selectionGroup[min];
+        selectionGroup[min] = aux;
+	}
+}
+
+function updateData(){
+    var input = document.getElementsByName('ubi[]');
+    for (var i = 0; i < input.length; i++) {
+        input[i].src = selectionGroup[i].scrImg;
+    }
+}
+
+function alertError(alg,group,order){
+    if ((alg === "") && (group === "") && (order === "")){
+        alert("Usuario " + userName + " ha dejado todas las casillas en blanco\nElija las opciones y intente nuevamente");
+    }else if((alg === "") && (group !== "") && (order !== "")){
+        alert("Usuario " + userName + " le ha faltado elegir el algoritmo de ordenamiento\nIntente nuevamente");
+    }else if((alg !== "") && (group === "") && (order !== "")){
+        alert("Usuario " + userName + " le ha faltado elegir el grupo de imagenes\nIntente nuevamente");
+    }else if((alg !== "") && (group !== "") && (order === "")){
+        alert("Usuario " + userName + " le ha faltado elegir el modo de ordenamiento\nIntente nuevamente");
+    }else if((alg === "") && (group === "") && (order !== "")){
+        alert("Usuario " + userName + " le ha faltado elegir el modo de ordenamiento y el grupo de imagenes\nIntente nuevamente");
+    }else if((alg !== "") && (group === "") && (order === "")){
+        alert("Usuario " + userName + " le ha faltado elegir el grupo de imagenes y el modo de ordenamiento\nIntente nuevamente");
+    }else if((alg === "") && (group !== "") && (order === "")){
+        alert("Usuario " + userName + " le ha faltado elegir el algoritmo de ordenamiento y el modo de ordenamiento\nIntente nuevamente");
+    }
+}
+
+
+function orderingProcess(alg,order){
+    if((alg === "1") && (order == "upward")){
+        bubbleUp();
+        alert("Se ordenará el grupo de imagenes de manera ascendente mediante ordenamiento burbuja");
+    }else if((alg === "1") && (order == "downward")){
+        bubbleDown();
+        alert("Se ordenará el grupo de imagenes de manera descendente mediante ordenamiento burbuja");
+    }else if((alg === "2") && (order == "upward")){
+        insertionUp();
+        alert("Se ordenará el grupo de imagenes de manera ascendente mediante ordenamiento por insersión");
+    }else if((alg === "2") && (order == "downward")){
+        insertionDown();
+        alert("Se ordenará el grupo de imagenes de manera descendente mediante ordenamiento por insersión");
+    }else if((alg === "3") && (order == "upward")){
+        selectionUp();
+        alert("Se ordenará el grupo de imagenes de manera ascendente mediante ordenamiento por selección");
+    }else if((alg === "3") && (order == "downward")){
+        selectionDown();
+        alert("Se ordenará el grupo de imagenes de manera descendente mediante ordenamiento por selección");
+    }
+    updateData();
+}
+
